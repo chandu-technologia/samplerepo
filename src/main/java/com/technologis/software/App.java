c3686491bfb5fe42ee8e03d06ea365ee44b83ba3
chandu-technologia <67130663+chandu-technologia@users.noreply.github.com>
2022-08-24 09:44:11 -0700

    Update App.java

--- src/main/java/com/technologis/software/App.java
+++ src/main/java/com/technologis/software/App.java
@@ -1,15 +1,33 @@
 package com.technologis.software;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import org.json.*;
+import com.mashape.unirest.http.*;
 
 public class App {
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
-
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
+  public static void main(String[] args) throws Exception {
+    for (String name: args) {
+      System.out.println("name: " + name);
+      String jsonString = new App(name).getUserInfoJSON();
+      JSONObject json = new JSONObject(jsonString);
+      System.out.println(json.toString(2));
+    }
   }
 
+  private final String name;
+
+  public App(String name) {
+    this.name = name;
+  }
+
+  public String getUserInfoURL() {
+    // Users | GitHub Developer Guide: https://developer.github.com/v3/users/
+    return "https://api.github.com/users/" + name;
+  }
+
+  public String getUserInfoJSON() throws Exception {
+    String url = getUserInfoURL();
+    HttpResponse<JsonNode> res = Unirest.get(url).asJson();
+    return res.getBody().toString();
+  }
 }
